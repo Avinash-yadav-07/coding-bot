f431a5f096d347f65d8e9634bf6725f68a5ea140
Avinash Yadav <ay9060645@gmail.com>
2025-01-15 21:25:02 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,20 +1,20 @@
-// Import the functions you need from the SDKs you need
+
 import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore"; // Import Firestore
+import { getFirestore } from "firebase/firestore"; 
 import { getAnalytics } from "firebase/analytics";
 
-// Your web app's Firebase configuration
+
 const firebaseConfig = {
   apiKey: "AIzaSyDFtT2nhIfs2LK-zDUrHFr92VakSaBqhMA",
   authDomain: "first-c362d.firebaseapp.com",
   projectId: "first-c362d",
-  storageBucket: "first-c362d.appspot.com", // Corrected the storage bucket URL
+  storageBucket: "first-c362d.appspot.com", 
   messagingSenderId: "961822509276",
   appId: "1:961822509276:web:baedce90b3d225f5220c19",
   measurementId: "G-XQ02YV11YY"
 };
 
-// Initialize Firebase
+
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 
